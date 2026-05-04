87f046acfa55f1c7cbe84bd6bcfe070c5b1799fa
DeanoUk8 <craig@mptransport.co.uk>
2026-05-04 19:50:50 +0100

    chore: remove OLDSYSTEM legacy Laravel app, MOBILE screenshots and stray PNGs
    These files served no purpose in the repository and were causing the
    zip build to emit thousands of 'could not open for reading' warnings
    due to Windows file locking across the Docker volume mount.

--- OLDSYSTEM/storage/framework/views/7f927bb09d6978c79f6ca1d8ec07b572.php
+++
@@ -1,6 +0,0 @@
-<head>
-    <?php echo $__env->make('partials.appcss', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
-</head>
-<?php echo $__env->yieldContent('content'); ?>
-<?php echo $__env->make('partials.appjs', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?>
-<?php echo $__env->yieldContent('scripts'); ?><?php /**PATH /home/mptportco/public_html/portal.mp-transport.co.uk/resources/views/layouts/form.blade.php ENDPATH**/ ?>
